ab4f76a62fbef2d846ee6efc7dbc77fa1afbf34d
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-02-18 19:41:04 +0300

    feat: add data sourse for cart and cart render function

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import { renderCart } from "./js/Cart";
+
+const data = [
+  {
+    name: "Vintage Backbag",
+    salePrice: 54.99,
+    price: 94.99,
+    onSale: true,
+    image: "src/img/photo1.png",
+    total: 1,
+  },
+  {
+    name: "Levi Shoes",
+    salePrice: 74.99,
+    price: 124.99,
+    onSale: true,
+    image: "src/img/photo2.png",
+    total: 1,
+  },
+];
+
+window.onload = function () {
+  const form = document.querySelector(".form");
+  const cartContainer = form.querySelector(".cart");
+
+  renderCart(cartContainer, data);
+};
