53e5e09e4d8c821bd620d7a7345fe44f16eaa250
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-02-19 11:22:30 +0300

    feat: add three type of messages: error, success, warning

--- src/js/Submit.js
+++ src/js/Submit.js
@@ -1,4 +1,4 @@
-import { showWarning, showSuccess } from "./utils";
+import { showMessage } from "./utils";
 
 const validateEmail = (email) => {
   return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(
@@ -10,27 +10,53 @@ const validatePhone = (phone) => {
   return /^\d[\d\(\)\ -]{4,14}\d$/.test(String(phone).toLowerCase());
 };
 
-const vadidateFields = (email, phone) => {
-  return validateEmail(email) && validatePhone(phone);
+const vadidateFields = ({ emailValue, phoneValue }) => {
+  return validateEmail(emailValue) && validatePhone(phoneValue);
+};
+
+const isFilled = (field) => {
+  return field.trim().length !== 0;
+};
+
+const checkValues = (fields) => {
+  for (const field of fields) {
+    if (!isFilled(field)) {
+      return false;
+    }
+  }
+  return true;
 };
 
 export const addListeners = () => {
   const submit = document.querySelector('input[type="submit"]');
   const form = document.querySelector(".form");
-  const emailValue = form.querySelector('input[name="user_email"]');
-  const phoneValue = form.querySelector('input[name="user_phone"]');
-  // const phoneValue = form.querySelector('input[name="user_phone"]');
-  // const phoneValue = form.querySelector('input[name="user_phone"]');
-  // const phoneValue = form.querySelector('input[name="user_phone"]');
-  // const phoneValue = form.querySelector('input[name="user_phone"]');
-  // const phoneValue = form.querySelector('input[name="user_phone"]');
   submit.addEventListener("click", (e) => {
     e.preventDefault();
+    const emailValue = form.querySelector('input[name="user_email"]').value;
+    const phoneValue = form.querySelector('input[name="user_phone"]').value;
+    const nameValue = form.querySelector('input[name="user_name"]').value;
+    const addressValue = form.querySelector('input[name="user_address"]').value;
+    const cityValue = form.querySelector('input[name="user_city"]').value;
+    const countries = form.querySelector('select[name="countries"]').value;
+    const postalCodeValue = form.querySelector('input[name="user_code"]').value;
 
-    if (vadidateFields(emailValue, phoneValue)) {
-      showSuccess();
+    const values = [
+      nameValue,
+      addressValue,
+      cityValue,
+      countries,
+      postalCodeValue,
+    ];
+    console.log(checkValues(values));
+    if (!checkValues(values)) {
+      showMessage("Please Fill All Required Field", "warning");
+      return;
+    }
+
+    if (vadidateFields({ emailValue, phoneValue })) {
+      showMessage("Your request is sent successfully!", "success");
     } else {
-      showWarning();
+      showMessage("Invalid email or phone number!", "error");
     }
   });
 };
